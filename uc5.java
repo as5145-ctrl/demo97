86d772fad92674ac0e3ace2bce54892985cadcc2
san_demo <as5145@srmist.edu.in>
2026-03-14 16:38:07 +0530

    commit

---
+++ uc5.java
@@ -0,0 +1,20 @@
+public class uc5 {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+            "  ***      ***    ******    *****  ",
+            " ** **    ** **   **   **  **   ** ",
+            "**   **  **   **  **   **  **      ",
+            "**   **  **   **  ******    *****  ",
+            "**   **  **   **  **             **",
+            " ** **    ** **   **       **   ** ",
+            "  ***      ***    **        *****  "
+        };
+
+        // Loop to print each line
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
