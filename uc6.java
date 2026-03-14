74ddaf5306727e09e2420910a6e1cae35309dcdf
san_demo <as5145@srmist.edu.in>
2026-03-14 16:27:37 +0530

    commit

---
+++ uc6.java
@@ -0,0 +1,22 @@
+public class uc6 {
+// Main method to run the banner display
+    public static void main(String[] args) {
+
+        // String array to store banner lines
+        String[] banner = {
+            "*******************************",
+            "*                             *",
+            "*        OOPS CONCEPTS        *",
+            "*                             *",
+            "*******************************"
+        };
+
+        // Loop through the array and print each line
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
+
+    
+
